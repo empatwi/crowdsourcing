3ed03e7652ecef632d8d77913e040474499c4c50
Fabiana Masini <masinifabiana@gmail.com>
2021-04-10 15:17:57 -0300

    Example API with CRUD working

--- backend/server.py
+++ backend/server.py
@@ -1,20 +1,43 @@
-from flask import Flask
-from flask_mongoengine import MongoEngine
+from flask import Flask, request, Response
+from database.db import initialize_db
+from database.models.movie import Movie
 
 app = Flask(__name__)
 
 app.config['MONGODB_SETTINGS'] = {
-    'db': 'empatwi_db',
-    'host': 'localhost',
-    'port': '27017'
+    'host': 'mongodb://localhost/movie-bag'
 }
 
-db = MongoEngine()
-db.init_app(app)
+initialize_db(app)
 
-@app.route("/")
-def home_page():
-    return "Home page"
+@app.route('/movies')
+def get_movies():
+    movies = Movie.objects().to_json()
+    return Response(movies, mimetype="application/json", status=200)
+
+@app.route('/movies', methods=['POST'])
+def add_movie():
+    body = request.get_json()
+    print(f"======= BODY: {body} =======")
+    movie = Movie(**body).save()
+    _id = movie.id
+    return {'id': str(_id)}, 200
+
+@app.route('/movies/<id>', methods=['PUT'])
+def update_movie(id):
+    body = request.get_json()
+    Movie.objects.get(id=id).update(**body)
+    return '', 200
+
+@app.route('/movies/<id>', methods=['DELETE'])
+def delete_movie(id):
+    Movie.objects.get(id=id).delete()
+    return '', 200
+
+@app.route('/movies/<id>')
+def get_movie(id):
+    movies = Movie.objects.get(id=id).to_json()
+    return Response(movies, mimetype="application/json", status=200)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
