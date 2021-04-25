0179d9b1fd3f45ec6a2122002067bd4e0ec22e45
Letícia Vigna <let.vigna@gmail.com>
2021-04-25 13:01:14 -0300

    Main scene exhibits tweet on tweet change via button

--- app/src/scenes/Main/index.js
+++ app/src/scenes/Main/index.js
@@ -1,3 +1,4 @@
+import { useMemo, useState } from 'react';
 import { Container } from '@material-ui/core';
 import ButtonComponent from '../../components/Button';
 import DividerComponent from '../../components/Divider';
@@ -6,11 +7,35 @@ import { colors } from '../../utils/colors';
 import { main } from '../../utils/texts';
 
 const styles = {
-  bottomTweetContainer: {
-    backgroundColor: 'black',
+  bottomContainer: {
+    alignItems: 'center',
+    display: 'flex',
     flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
   },
-  contentContainer: {
+  divider: {
+    height: '100px',
+  },
+  padding: {
+    padding: 50,
+  },
+  subTopText: {
+    fontSize: 16,
+    marginTop: 0,
+  },
+  topText: {
+    fontSize: 40,
+    fontWeight: 'bold',
+    marginBottom: 0,
+    marginTop: 0,
+  },
+  tweet: {
+    color: colors.gray,
+    fontSize: 16,
+    textAlign: 'center',
+  },
+  tweetContainer: {
     alignItems: 'stretch',
     backgroundColor: colors.white,
     border: '4px solid',
@@ -19,50 +44,56 @@ const styles = {
     display: 'flex',
     flexDirection: 'column',
     height: '60%',
-    width: '80%',
+    width: '70%',
   },
-  divider: {
-    backgroundColor: 'black',
-    height: '100px',
+  tweetContainerBottom: {
+    alignItems: 'center',
+    display: 'flex',
+    flex: 1,
+    justifyContent: 'center',
+    padding: 8,
   },
-  padding: {
-    padding: 50,
-  },
-  subTopText: {
-    fontSize: 16,
-    margin: 0,
-  },
-  topText: {
-    fontSize: 40,
-    fontWeight: 'bold',
-    marginBottom: 0,
-    marginTop: 0,
-  },
-  topTweetContainer: {
-    backgroundColor: 'pink',
+  tweetContainerTop: {
     height: '20%',
   },
 };
 
 export default function Main() {
+  const [tweet, setTweet] = useState(null);
+  console.log(tweet);
+
   const onClick = async () => {
-    const res = await getTweet();
-    console.log('getTweet res', res);
+    const returnedTweet = await getTweet();
+    setTweet(
+      returnedTweet?.tweet_content && returnedTweet?.tweet_content?.length > 0
+        ? returnedTweet?.tweet_content
+        : null,
+    );
   };
+
+  const renderTweetBottom = useMemo(() => {
+    return (
+      <div style={styles.tweetContainerBottom}>
+        <p style={styles.tweet}>{tweet ? tweet : 'Mensagem nulo'}</p>
+      </div>
+    );
+  }, [tweet]);
 
   return (
     <>
       <p style={styles.topText}>{main.topText}</p>
       <p style={styles.subTopText}>{main.subTopText}</p>
-      <ButtonComponent onClick={onClick} text="API test :D" />
-      <Container
-        disableGutters
-        maxWidth={false}
-        style={styles.contentContainer}
-      >
-        <div style={styles.topTweetContainer}>top</div>
-        <DividerComponent />
-        <div style={styles.bottomTweetContainer}>bottom</div>
+      <Container disableGutters maxWidth={false} style={styles.bottomContainer}>
+        <ButtonComponent onClick={onClick} text="API test :D" />
+        <Container
+          disableGutters
+          maxWidth={false}
+          style={styles.tweetContainer}
+        >
+          <div style={styles.tweetContainerTop}>top</div>
+          <DividerComponent />
+          {renderTweetBottom}
+        </Container>
       </Container>
     </>
   );
