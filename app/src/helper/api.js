650f8428cc7fca085c817638dc5075c757e9d45c
Letícia Vigna <let.vigna@gmail.com>
2021-04-25 13:00:52 -0300

    Fetch tweet returns request data

--- app/src/helper/api.js
+++ app/src/helper/api.js
@@ -1,35 +1,15 @@
 import axios from 'axios';
 
 export const getTweet = async () => {
-  /* try {
-    // eslint-disable-next-line no-undef
-    const urlString = `${process.env.REACT_APP_API_URL}/tweet/`;
-    const response = await axios({
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      method: 'GET',
-      url: urlString,
-    });
-    console.log('response', response);
-  } catch (error) {
-    console.error('error', error);
-  } */
-
   // eslint-disable-next-line no-undef
   const urlString = `${process.env.REACT_APP_API_URL}/tweet/`;
-  await axios({
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    method: 'GET',
-    url: urlString,
-  })
+  return await axios({ method: 'GET', url: urlString })
     .then(res => {
-      console.log('res', res);
+      if (res?.status === 200 && res?.data?.length === 1) return res?.data[0];
+      return null;
     })
     .catch(err => {
-      console.log('err', err);
-      return 'catch';
+      console.error(err);
+      return null;
     });
 };
