b47badc0ca82072a1be6d090c280f9f373900f30
Letícia Vigna <let.vigna@gmail.com>
2021-04-25 20:08:32 -0300

    Updated tweet parser to include id

--- app/src/utils/index.js
+++ app/src/utils/index.js
@@ -1,9 +1,12 @@
 // https://t.co/UODb0dJZ7W, https://t.co/KIVi8BPXrG
 export const parseTweet = tweet => {
-  if (tweet?.tweet_content) {
-    let { tweet_content: content } = tweet;
+  if (tweet?._id && tweet?.tweet_content) {
     const regex = /https:\/\/t.co\/.*$/;
-    return content.replace(regex, '');
+    const returnObject = {
+      id: String(tweet?._id),
+      tweet_content: tweet?.tweet_content?.replace(regex, ''),
+    };
+    return returnObject;
   }
   return null;
 };
