a82a71b85deb46899b39b4eca8eecfbe6a2345a4
Letícia Vigna <let.vigna@gmail.com>
2021-05-05 01:08:10 -0300

    Updated app structure to Router

--- app/src/App.js
+++ app/src/App.js
@@ -1,39 +1,25 @@
 import './App.css';
-import { useState } from 'react';
-import { Container } from '@material-ui/core';
-import FooterComponent from './components/Footer';
-import HeaderComponent from './components/Header';
-import Body from './scenes';
-import { colors } from './utils/colors';
-import { footer, header } from './utils/texts';
-
-const styles = {
-  main: {
-    backgroundColor: colors.lightGreen,
-    display: 'flex',
-    flexDirection: 'column',
-    height: '100vh',
-    width: '100vw',
-  },
-};
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
 
 function App() {
-  const [selectedTab, setSelectedTab] = useState(header.tabs[0]);
-
-  const handleChange = value => {
-    setSelectedTab(value);
-  };
+  const Home = () => <h1>Home</h1>;
+  const About = () => <h1>About</h1>;
+  const Services = () => <h1>Services</h1>;
+  const Contact = () => <h1>Contact</h1>;
+  const SignUp = () => <h1>SignUp</h1>;
 
   return (
-    <Container disableGutters maxWidth={false} style={styles.main}>
-      <HeaderComponent
-        handleTabChange={handleChange}
-        selectedTab={selectedTab}
-        tabs={header.tabs}
-      />
-      <Body page={selectedTab} />
-      <FooterComponent items={footer.links} text={footer.text} />
-    </Container>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route component={Home} exact path="/" />
+        <Route component={About} path="/about" />
+        <Route component={Services} path="/services" />
+        <Route component={Contact} path="/contact-us" />
+        <Route component={SignUp} path="/sign-up" />
+      </Switch>
+    </Router>
   );
 }
 
