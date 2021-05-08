47a2593698dde84d1400e3ee3883326abf912b22
Letícia Vigna <let.vigna@gmail.com>
2021-05-07 23:53:09 -0300

    Updated contact information

--- app/src/utils/index.js
+++ app/src/utils/index.js
@@ -1,4 +1,5 @@
 import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import TwitterIcon from '@material-ui/icons/Twitter';
 import About from '../pages/about';
 import Contact from '../pages/contact';
@@ -19,6 +20,40 @@ export const main = {
   topText: 'Contribua com o empatwi',
   tweetTopText: 'Qual é o sentimento deste comentário?',
 };
+
+export const contacts = [
+  {
+    desc: 'Back end',
+    image: '/images/fabiana.jpg',
+    links: [
+      { icon: <TwitterIcon />, url: 'https://twitter.com/fabianamasini' },
+      {
+        icon: <LinkedInIcon />,
+        url: 'https://www.linkedin.com/in/fabianamasini',
+      },
+      {
+        icon: <GitHubIcon />,
+        url: 'https://github.com/fabianamasini',
+      },
+    ],
+    name: 'Fabiana Masini',
+  },
+  {
+    desc: 'Front end',
+    image: '/images/leticia.jpg',
+    links: [
+      {
+        icon: <LinkedInIcon />,
+        url: 'https://www.linkedin.com/in/leticia-vigna',
+      },
+      {
+        icon: <GitHubIcon />,
+        url: 'https://github.com/letvigna',
+      },
+    ],
+    name: 'Letícia Vigna',
+  },
+];
 
 export const footer = {
   links: [
