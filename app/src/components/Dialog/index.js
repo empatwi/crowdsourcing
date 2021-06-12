7e19bf052d6565c15c6c524e52b61253dd3a7a1f
Letícia Vigna <let.vigna@gmail.com>
2021-06-12 18:57:41 -0300

    Cleaned unused code

--- app/src/components/Dialog/index.js
+++ app/src/components/Dialog/index.js
@@ -1,11 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
-import { makeStyles } from '@material-ui/core/styles';
 import PropTypes from 'prop-types';
 import { main } from '../../utils/content';
 import { colors } from '../../utils/theme';
@@ -27,18 +26,6 @@ const styles = {
     color: colors.buttonGreen,
   },
 };
-
-const useStyles = makeStyles(theme => ({
-  root: {
-    backgroundColor: theme.palette.background.paper,
-    maxWidth: 360,
-    width: '100%',
-  },
-  paper: {
-    maxHeight: 435,
-    width: '80%',
-  },
-}));
 
 export default function ConfirmationDialog({
   handleCancel,
